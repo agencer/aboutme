f285480d1a1fa63427413d153df895641ff83fcb
agencer <47377400+agencer@users.noreply.github.com>
2026-05-02 06:01:21 +0000

    Deploying to gh-pages from @ agencer/aboutme@160924db2779201bf08cf71ff70a0ea2d777fccd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "Download my full academic CV.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Courses taught and teaching experience.",
